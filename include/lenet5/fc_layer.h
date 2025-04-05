979d7cb403cf1c9ea3cca57213ad0c49ba5d60dd
megankuo <megankuo@asu.edu>
2025-04-05 12:51:00 -0700

    impl fc layer test

--- include/lenet5/fc_layer.h
+++ include/lenet5/fc_layer.h
@@ -6,14 +6,39 @@
 
 typedef ap_fixed<8, 3> data_t;
 
-template<int OUT_DIM, IN_DIM>
+template<int OUT_DIM, int IN_DIM>
 void fc_layer(
         hls::stream<data_t>& in_stream,
         hls::stream<data_t>& out_stream,
-        const data_t weights[OUT_DIM][IN_DIM],
-        data_t bias[OUT_DIM],
+        const data_t weight[OUT_DIM][IN_DIM],
+        const data_t bias[OUT_DIM],
         bool use_relu
-        );
+        ) {
+#pragma HLS INLINE OFF
+#pragma HLS ARRAY_PARTITION variable=weight cyclic factor=8 dim=2
+#pragma HLS ARRAY_PARTITION variable=bias complete dim=1
 
-data_t relu(data_t x);
+    data_t ret[OUT_DIM];
+    for(int j=0; j<OUT_DIM; ++j) {
+        ret[j] = bias[j];
+    }
+
+    for(int i=0; i<IN_DIM; ++i) {
+        data_t val = in_stream.read();
+        for(int j=0; j<OUT_DIM; ++j) {
+#pragma HLS UNROLL factor=4
+            ret[j] += weight[j][i] * val;
+        }
+    }
+
+    for(int j=0; j<OUT_DIM; ++j) {
+        data_t result = use_relu ? relu(ret[j]) : ret[j];
+        out_stream.write(result);
+    }
+}
+
+data_t relu(data_t x) {
+    return x > 0 ? x : data_t(0);
+}
+
 #endif
