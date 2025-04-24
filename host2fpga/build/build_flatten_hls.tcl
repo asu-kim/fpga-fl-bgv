27dd2aa1764717f6912460678eb65bd5430ae78e
megankuo <megankuo@asu.edu>
2025-04-24 12:56:15 -0700

    [Build] move build tcl logic to build/

---
+++ host2fpga/build/build_flatten_hls.tcl
@@ -0,0 +1,19 @@
+open_project flatten_hls
+set_top flatten_kernel
+
+add_files kernels/flatten_kernel.cpp
+add_files kernels/aes_utils.h
+
+open_solution sol1 -flow_target vitis
+
+set_part  xcvu37p-fsvh2892-2L-e
+create_clock -period 3.0 -name default
+config_compile -pipeline_loops 1     
+
+csynth_design
+export_design \
+    -rtl verilog \
+    -format ip_catalog \
+    -output flatten_ip \
+    -ipname flatten_kernel
+exit
